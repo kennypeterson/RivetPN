a9c63d43f9267e774e1834e7b01cba08e3da5879
kenny.peterson <kenny.peterson@me.com>
2014-05-12 11:55:44 -0400

    Found Acceptable difference
    only usable for reference

--- Rivet PN.vb
+++ Rivet PN.vb
@@ -4,12 +4,17 @@
     Private oMaterialThickness As Decimal
     Private oRivetModelLength As Decimal
     Private oRivetPNLength As Decimal
-    Private oAcceptableDifference As Decimal
+    Private oRivetSuggestedLength As Decimal
+    Private oAcceptableDifference As Decimal = 0.75
     Private oMaterialUsed As Decimal
     Private oEndDashNumber As String
 
 
     Sub main()
+
+        oShankDiameter * 1.5 = oRivetSuggestedLength
+
+
 
 
 
