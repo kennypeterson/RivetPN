ba1943b2499585ce2378649d895f4cb09f7f5148
kenny.peterson <kenny.peterson@me.com>
2014-05-12 13:57:59 -0400

    Added lists for dash numbers

--- Rivet PN.vb
+++ Rivet PN.vb
@@ -1,4 +1,73 @@
 ﻿'Imports Inventor
+Imports System.Collections.Generic
+
+Public Class dash1
+
+    Implements IEquatable(Of dash1)
+
+    Shared Property FirstDash As String
+        Get
+            Return m_firstdash
+        End Get
+        Set(value As String)
+            m_firstdash = value
+        End Set
+    End Property
+
+    Shared Property value1 As String
+        Get
+            Return m_value1
+        End Get
+        Set(value As String)
+            m_value1 = value
+        End Set
+    End Property
+
+    Private Shared Property m_firstdash As String
+
+    Private Shared Property m_value1 As String
+
+    Public Function Equals1(other As dash1) As Boolean
+
+
+    End Function
+End Class
+
+Public Class dash2
+
+    Implements IEquatable(Of dash2)
+
+    Shared Property secondDash As String
+        Get
+            Return m_seconddash
+        End Get
+        Set(value As String)
+            m_seconddash = value
+        End Set
+    End Property
+
+    Shared Property value2 As String
+        Get
+            Return m_value2
+        End Get
+        Set(value As String)
+            m_value2 = value
+        End Set
+    End Property
+
+    Private Shared Property m_firstdash As String
+
+    Private Shared Property m_value1 As String
+
+    Private Shared Property m_value2 As String
+
+    Private Shared Property m_seconddash As String
+
+    Public Function Equals1(other As dash1) As Boolean
+
+
+    End Function
+End Class
 
 Public Class main
 
@@ -9,13 +78,19 @@ Public Class main
     Private oRivetSuggestedLength As Decimal
     Private oAcceptableDifference As Decimal = 0.562
     Private oMaterialUsed As Decimal '= MaterialUsed
-    Private oEndDashNumber As String 'create array? for dash lengths
+    Private oEndDashNumber As New List(Of String)
+    Private oBasePN As String '= iProperties.Value("Project", "Part Number")
+    Private oFirstDashNumber As New List(Of String)
+    Private oCompletePN As String
 
 
     Sub main()
 
         oRivetSuggestedLength = oShankDiameter * 1.5
         oRivetModelLength = oMaterialThickness
+
+        Dim parts As New List(Of main)
+
 
         'the first two if statements test the length of the rivet to make sure it won't be too long or too short
         If oRivetPNLength > oAcceptableDifference + oRivetSuggestedLength Then
@@ -25,6 +100,9 @@ Public Class main
         Else
             '
         End If
+
+        'match concatenation to Inventor iPart table
+        oCompletePN = oBasePN & oFirstDashNumber & oEndDashNumber
 
 
     End Sub
